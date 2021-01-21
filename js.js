0dd044469a4888c8a469264d30a10cb6a6e166bd
JmGarcia <josemanuelgarciamunoz@gmail.com>
2021-01-21 13:47:16 +0100

    Colorizeme

---
+++ js.js
@@ -0,0 +1,87 @@
+function changeColorCuerpo(color) {
+    var color = color.className;
+    var cuerpo = document.getElementById('cuerpo'); 
+    cuerpo.style.backgroundColor = color;
+}
+
+function changeColorPantalones(color) {
+    var color = color.className;
+    var pantalones = document.getElementById('pantalones'); 
+    pantalones.style.backgroundColor = color;
+}
+
+function changeColorZapatillas(color) {
+    var color = color.className;
+    var zapatillas = document.getElementById('zapatillas'); 
+    zapatillas.style.backgroundColor = color;
+}
+
+function conjuntar(porcentaje) {    
+    var cuerpo = document.getElementById('cuerpo');
+    var pantalones = document.getElementById('pantalones');
+    var zapatillas = document.getElementById('zapatillas');    
+    var colorCuerpo = cuerpo.style.backgroundColor;  
+    var colorPantalones = pantalones.style.backgroundColor;  
+    var colorZapatillas = zapatillas.style.backgroundColor;  
+
+    if ((colorCuerpo == "darkblue") && (colorPantalones == "black") || (colorCuerpo == "black") && (colorPantalones == "darkblue")) {
+        alert("¿Azul con Negro? ¡No por favor!");
+    } else if ((colorCuerpo == "darkblue") && (colorPantalones == "darkblue")) {
+        alert("Pareces un pitufo. ¡No gracias!");
+    } else if ((colorCuerpo == "black") && (colorPantalones == "black")) {
+        alert("¡Pon un poco de color!, aunque se respeta si te gusta ir de negro");
+    } else if ((colorCuerpo == "brown") && (colorPantalones == "brown")) {
+        alert("Tanto rojo parece que vienes de asesinar a alguien y no, no queremos que piensen eso.");
+    } else if ((colorCuerpo == "darkblue") && (colorPantalones == "beige") && (colorZapatillas !== "white")) {
+        alert("Me parece bien. Prueba con unas zapatillas blancas");  
+    } else if ((colorCuerpo == "darkblue") && (colorPantalones == "beige") && (colorZapatillas == "white")) {
+        alert("Me gusta tu estilo");                  
+    } else {
+        alert("Feel free.");
+    }
+}
+
+function generarNuevoColorCuerpo(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+    }    
+    var cuerpo = document.getElementById('cuerpo');         
+    cuerpo.style.background = color;	
+}
+
+function generarNuevoColorPantalones(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+    }        
+    var pantalones = document.getElementById('pantalones');      
+    pantalones.style.background = color;    
+}
+
+function generarNuevoColorZapatillas(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+    }       
+    var zapatillas = document.getElementById('zapatillas');      
+    zapatillas.style.background = color;    
+}
+
+function reset(color) {    
+    var cuerpo = document.getElementById('cuerpo');
+    var pantalones = document.getElementById('pantalones'); 
+    var zapatillas = document.getElementById('zapatillas');
+    cuerpo.style.backgroundColor = "burlywood";
+    pantalones.style.backgroundColor = "burlywood";
+    zapatillas.style.backgroundColor = "burlywood";
+}
